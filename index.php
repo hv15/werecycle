b218a5f558e90634296091f1ebf0a7d31fff92d7
Leif Gehrmann <leif.gehrmann@gmail.com>
2012-09-15 18:36:16 +0100

    added geocoding to index.php

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 		<title>RecycleFinder</title>
 		<link type="text/css" rel="stylesheet" href="styles/main.css"/>
 		<link type="text/css" rel="stylesheet" href="styles/index.css"/>
+		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
 		<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=true"></script>
 		<script type="text/javascript" src="scripts/index.js"></script>
 	</head>
@@ -14,9 +15,9 @@
 			<div id="Name">weRecycle</div>
 			<div id="Info"><p>Enter in your location or use geolocation.</p></div>
 			<div id="GeoLocationButton" class="button" onclick="getGeoLocation()"><p>Use My Location</p></div>
-			<div id="GeoName">
-				<div id="GeoNameInput" class="input left"><input type="text"/></div>
-				<div id="GeoNameButton" class="button left" onclick="getGeoName()"><p>Search</p></div>
+			<div id="GeoCode">
+				<div id="GeoCodeInput" class="input left"><input type="text"/></div>
+				<div id="GeoCodeButton" class="button left" onclick="getGeoCode()"><p>Search</p></div>
 			</div>
 		</div>
 	</body>
--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,7 @@
+var geocoder;
+/*
+	Deals with the geolocation
+*/
 function getGeoLocation(){
 	if (navigator.geolocation) {
 		var timeoutVal = 10 * 1000 * 1000;
@@ -11,11 +15,25 @@ function getGeoLocation(){
 		alert("Geolocation is not supported by this browser");
 	}
 }
-
 function returnPosition(position) {
-	alert("./map.php?latitude="+position.coords.latitude+"&longitude="+position.coords.longitude);
 	window.location.href = "./map.php?latitude="+position.coords.latitude+"&longitude="+position.coords.longitude;
 }
 function returnError(position) {
   alert("Geolocation did not work");
+}
+
+
+/*
+	Deals with geocoding
+*/
+function getGeoCode() {
+	var address = document.getElementById('address').value;
+	geocoder.geocode( { 'address': address}, function(results, status) {
+		if (status == google.maps.GeocoderStatus.OK) {
+			window.location.href = "./map.php?latitude="+results[0].geometry.location.lat()+"&longitude="+results[0].geometry.location.lng();
+			results[0].geometry.location.lat();
+		} else {
+			alert('Geocode was not successful for the following reason: ' + status);
+		}
+    });
 }
