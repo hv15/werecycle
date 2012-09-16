b3439cead47089a5b6097222568149a4a1ea2ed1
Leif Gehrmann <leif.gehrmann@gmail.com>
2012-09-16 12:51:07 +0100

    added select.php and the other jazz (css, js)

--- select.php
+++ select.php
@@ -2,23 +2,18 @@
 	<head>
 		<meta name="viewport" content="initial-scale = 1.0,maximum-scale = 1.0,user-scalable=no,width=device-width" />
 		<meta http-equiv="content-type" content="text/html;charset=utf-8" />
-		<title>RecycleFinder</title>
+		<title>RecycleFinder : Select Recyclables</title>
 		<link type="text/css" rel="stylesheet" href="styles/main.css"/>
-		<link type="text/css" rel="stylesheet" href="styles/index.css"/>
-		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
-		<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=true"></script>
-		<script type="text/javascript" src="scripts/index.js"></script>
+		<link type="text/css" rel="stylesheet" href="styles/select.css"/>
+		<script type="text/javascript" src="scripts/selectables.js"></script>
+		<script type="text/javascript" src="scripts/selectablesInfo.js"></script>
+		<script type="text/javascript" src="scripts/select.js"></script>
 	</head>
-	<body>
-		<div id="Index">
-			<img id="Icon" src="graphics/logo.png"/>
-			<div id="Name">weRecycle</div>
-			<div id="Info"><p>Enter in your location or use geolocation.</p></div>
-			<div id="GeoLocationButton" class="button" onclick="getGeoLocation()"><p>Use My Location</p></div>
-			<div id="GeoCode">
-				<div id="GeoCodeInput" class="input left"><input type="text" id="GeoCodeInputField"/></div>
-				<div id="GeoCodeButton" class="button left" onclick="getGeoCode()"><p>Search</p></div>
-			</div>
+	<body onload="loadSelectables(selectables,selectablesInfo);">
+		<div id="Select">
+			<div id="Info"><p>Select the items you want to recycle</p></div>
+			<div id="Container"></div>
+			<div id="SelectButton" class="button left" onclick="createQuery();"><p>Search recycling facilities...</p></div>
 		</div>
 	</body>
 </html>
