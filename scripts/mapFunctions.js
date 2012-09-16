e07771d865e46ddc2ed209de347c211f9c22c91a
Hans <hansthespass@googlemail.com>
2012-09-16 19:36:53 +0100

    JS

--- scripts/mapFunctions.js
+++ scripts/mapFunctions.js
@@ -92,7 +92,9 @@ function toggleLocation(){
 
 function drawMarkers(newlocation) {
 	// get dynamically the JSON data via data.php for the markers
-	$.getJSON("data.php?longitude="+newlocation.lng()+"&longitude="+newlocation.lat()+"&types=6", function(data) {
+	var urly = "data.php?longitude="+newlocation.lng()+"&longitude="+newlocation.lat()+"&types=6";
+	console.log(urly);
+	$.getJSON(urly, function(data) {
 		this.data = data;
 		console.log(data);
 	});
