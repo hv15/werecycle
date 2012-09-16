9d29dfb37329d0d9816797068b3f0747e172d5e7
Hans <hansthespass@googlemail.com>
2012-09-16 19:30:08 +0100

    JS fix, bad syntax

--- scripts/mapFunctions.js
+++ scripts/mapFunctions.js
@@ -164,7 +164,7 @@ function initialize(){
 	google.maps.event.addListener(map, 'center_changed', function() {
 		var newlocation = map.getCenter();
 		drawMarkers(newlocation);
-	}
+	});
 	// Create the graphics that we will use
 	//var recyclePointMarkerImage = new google.maps.MarkerImage(recyclePointIcon , new google.maps.Size(64, 64), new google.maps.Point(0, 0), new google.maps.Point(32, 32), new google.maps.Size(64, 64));
 	//var recycleCenterMarkerImage = new google.maps.MarkerImage(recycleCenterIcon, new google.maps.Size(64, 64), new google.maps.Point(0, 0), new google.maps.Point(32, 32), new google.maps.Size(64, 64));
