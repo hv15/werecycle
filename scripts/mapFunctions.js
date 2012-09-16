dabeee243910057a33664c99af78ce7f930b9879
Hans <hansthespass@googlemail.com>
2012-09-16 19:44:11 +0100

    JS

--- scripts/mapFunctions.js
+++ scripts/mapFunctions.js
@@ -94,7 +94,7 @@ function drawMarkers(newlocation) {
 	// get dynamically the JSON data via data.php for the markers
 	var urly = "data.php?longitude="+newlocation.lng()+"&latitude="+newlocation.lat()+"&types=6";
 	alert("BEFORE");
-	$.getJSON(urly, function(check) {
+	jQuery.getJSON(urly, function(check) {
 		alert(check);
 		console.log(check);
 	});
