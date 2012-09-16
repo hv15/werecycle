a58e0a240f2a93ae1e0ab607c56b8c114f9a115d
Andrew Beveridge <andrew.d.beveridge@gmail.com>
2012-09-16 18:50:26 +0100

    info.php now standalone page

--- info.php
+++ info.php
@@ -1,3 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta name="viewport" content="initial-scale = 1.0,maximum-scale = 1.0,user-scalable=no,width=device-width" />
+		<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=9"/>
+		<title>RecycleFinder</title>
+		<link type="text/css" rel="stylesheet" href="styles/main.css"/>
+		<link type="text/css" rel="stylesheet" href="styles/index.css"/>
+		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
+		<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=true"></script>
+		<script type="text/javascript" src="scripts/index.js"></script>
+	</head>
+	<body>
+		<div id="Info">
 <?php      
     // Connect to database
     mysql_connect(localhost,"recycle_finder","leifgivesyoulemons");
@@ -55,4 +70,6 @@
     }
 ?>
 
-    
+		</div>
+	</body>
+</html>
