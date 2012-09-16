e593a0415aaaa40987d4f028a5a4737b29382ba8
Andrew Beveridge <andrew.d.beveridge@gmail.com>
2012-09-17 00:52:41 +0100

    fixed application path in controller

--- app/controllers/pages.php
+++ app/controllers/pages.php
@@ -4,7 +4,7 @@ class Pages extends CI_Controller {
 
 	public function view($page = 'home')
 	{				
-		if ( ! file_exists('application/views/pages/'.$page.'.php'))
+		if ( ! file_exists('app/views/pages/'.$page.'.php'))
 		{
 			// Whoops, we don't have a page for that!
 			show_404();
