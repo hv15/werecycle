5fb8ea86d09173233fec2624e9ab84c985a9672b
Leif Gehrmann <leif.gehrmann@gmail.com>
2012-09-17 21:09:32 +0100

    changed css for info.php

--- app/views/pages/info.php
+++ app/views/pages/info.php
@@ -1,10 +1,10 @@
 	<div class="main-container">
             <div class="main wrapper clearfix">
-		<?php echo $info; ?>
-		<div id="ButtonInfoBack" class="button">
-			<p>
-				<a href="/map">Back to Map</a>
-			</p>
-		</div>
+				<?php echo $info; ?>
+				<a href="/map">
+					<div id="ButtonInfoBack" class="button">
+						<p>Back to Map</p>
+					</div>
+				</a>
             </div> <!-- #main -->
         </div> <!-- #main-container -->
