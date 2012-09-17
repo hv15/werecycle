a3b21e3846238b7ba01ba69b35a4a850b09832df
Andrew Beveridge <andrew.d.beveridge@gmail.com>
2012-09-17 12:29:54 +0100

    implementing sessions 3

--- app/views/scripts/select.js.php
+++ app/views/scripts/select.js.php
@@ -1,5 +1,5 @@
 recalculateSelected();
-console.log(eval($.cookie('recyclefinder_session')));
+console.log(readCookie('recyclefinder_session'));
 	
 function recalculateSelected() {
 	$('.category').each(function(index) {
@@ -25,10 +25,27 @@ $('.typeCheckbox').change(function() {
 	recalculateSelected();
 });
 
+function createCookie(name,value,days) {
+	if (days) {
+		var date = new Date();
+		date.setTime(date.getTime()+(days*24*60*60*1000));
+		var expires = "; expires="+date.toGMTString();
+	}
+	else var expires = "";
+	document.cookie = name+"="+value+expires+"; path=/";
+}
+
 function readCookie(name) {
-alert('reading cookie with name: '+name);
-    name += '=';
-    for (var ca = document.cookie.split(/;\s*/), i = ca.length - 1; i >= 0; i--)
-        if (!ca[i].indexOf(name))
-            return ca[i].replace(name, '');
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0;i < ca.length;i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1,c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+	}
+	return null;
+}
+
+function eraseCookie(name) {
+	createCookie(name,"",-1);
 }
