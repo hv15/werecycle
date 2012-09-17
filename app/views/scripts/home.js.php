746784d57f3c031b9b22db7178fb20c44c1bc905
Andrew Beveridge <andrew.d.beveridge@gmail.com>
2012-09-17 11:31:56 +0100

    implementing sessions 2

--- app/views/scripts/home.js.php
+++ app/views/scripts/home.js.php
@@ -1,62 +1,59 @@
-$.getScript("http://maps.google.com/maps/api/js?sensor=true", function(){
+var geocoder;
 
-	var geocoder;
-
-	document.onkeydown = checkKeycode;
-	/*
-		Deals with the geolocation
-	*/
-	function getGeoLocation(){
-		spin("visible");
-		if (navigator.geolocation) {
-			var timeoutVal = 10000;
-			navigator.geolocation.getCurrentPosition(
-				returnPosition, 
-				returnError,
-				{ enableHighAccuracy: true, timeout: timeoutVal, maximumAge: 0 }
-			);
-		}
-		else {
-			spin("invisible");
-			alert("Geolocation is not supported by this browser");
-		}
+document.onkeydown = checkKeycode;
+/*
+	Deals with the geolocation
+*/
+function getGeoLocation(){
+	spin("visible");
+	if (navigator.geolocation) {
+		var timeoutVal = 10000;
+		navigator.geolocation.getCurrentPosition(
+			returnPosition, 
+			returnError,
+			{ enableHighAccuracy: true, timeout: timeoutVal, maximumAge: 0 }
+		);
 	}
-	function returnPosition(position) {
-		window.location.href = "/select/"+position.coords.latitude+"/"+position.coords.longitude;
-	}
-	function returnError(position) {
+	else {
 		spin("invisible");
-		alert("Geolocation did not work");
+		alert("Geolocation is not supported by this browser");
 	}
+}
+function returnPosition(position) {
+	window.location.href = "/select/"+position.coords.latitude+"/"+position.coords.longitude;
+}
+function returnError(position) {
+	spin("invisible");
+	alert("Geolocation did not work");
+}
 
 
-	/*
-		Deals with geocoding
-	*/
-	function getGeoCode() {
-		geocoder = new google.maps.Geocoder();
-		var address = document.getElementById('geoCodeInputField').value;
-		geocoder.geocode( { 'address': address}, function(results, status) {
-			if (status == google.maps.GeocoderStatus.OK) {
-				window.location.href = "/select/"+results[0].geometry.location.lat()+"/"+results[0].geometry.location.lng();
-			} else {
-				alert("Could not find that location. Try formatting in another way.");
-			}
-	    });
-	}
+/*
+	Deals with geocoding
+*/
+function getGeoCode() {
+	geocoder = new google.maps.Geocoder();
+	var address = document.getElementById('geoCodeInputField').value;
+	geocoder.geocode( { 'address': address}, function(results, status) {
+		if (status == google.maps.GeocoderStatus.OK) {
+			window.location.href = "/select/"+results[0].geometry.location.lat()+"/"+results[0].geometry.location.lng();
+		} else {
+			alert("Could not find that location. Try formatting in another way.");
+		}
+    });
+}
 
-	function spin(state){type="text/javascript" 
-		var spin = document.getElementById("geoLocationSpin");
-		spin.className = state;
-	}
+function spin(state){type="text/javascript" 
+	var spin = document.getElementById("geoLocationSpin");
+	spin.className = state;
+}
 
-	function checkKeycode(e) {
-		var keycode;
-		if (window.event)
-			keycode = window.event.keyCode;
-		else if (e)
-			keycode = e.which;
-		if(keycode==13)
-			getGeoCode();
-	}
+function checkKeycode(e) {
+	var keycode;
+	if (window.event)
+		keycode = window.event.keyCode;
+	else if (e)
+		keycode = e.which;
+	if(keycode==13)
+		getGeoCode();
 }
