d7e4d86288cf28f74d5847192dd9f9d3b9c85911
Andrew Beveridge <andrew.d.beveridge@gmail.com>
2012-09-17 05:08:32 +0100

    implemented get_categories function in map model and select view

--- app/controllers/pages.php
+++ app/controllers/pages.php
@@ -4,7 +4,6 @@ class Pages extends CI_Controller {
 	public function __construct()
 	{
 	    parent::__construct();
-	    $this->load->library('session');
 	    $this->load->model('map_model');
 	}
 
