62092ee5c4f76f4d048f38747b5212df97e67774
Andrew Beveridge <andrew.d.beveridge@gmail.com>
2012-09-17 16:08:38 +0100

    made help page

---
+++ app/views/pages/help.php
@@ -0,0 +1,5 @@
+	<div class="main-container">
+            <div class="main wrapper clearfix">
+		Hello world, we are awesome!
+            </div> <!-- #main -->
+        </div> <!-- #main-container -->
