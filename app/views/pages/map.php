093332c85ac6554dd6f01334832bac3529481e0d
Andrew Beveridge <andrew.d.beveridge@gmail.com>
2012-09-17 16:08:28 +0100

    made help page

--- app/views/pages/map.php
+++ app/views/pages/map.php
@@ -2,5 +2,5 @@
 	<div id="ButtonZoomIn" class="button roundTop" onclick="zoomIn()"><p>+</p></div>
 	<div id="ButtonZoomOut" class="button roundBottom" onclick="zoomOut()"><p>&#8211;</p></div>
 	<div id="ButtonLocation" class="button inactive" onclick="toggleLocation()"><p><img src="/img/location.png" width="22"/></p></div>
-	<a href="./help.php"><div id="ButtonHelp" class="button"><p>?</p></div></a>
+	<a href="/help"><div id="ButtonHelp" class="button"><p>?</p></div></a>
 	<a href="#" onclick="buttonSelect();"><div id="ButtonSelect" class="button"><p>Select Recyclables...</p></div></a>
