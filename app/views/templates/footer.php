811d2d3fea2995c5f0778c3972403519e359cec7
Andrew Beveridge <andrew.d.beveridge@gmail.com>
2012-09-17 03:52:31 +0100

    added google api script to footer

--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -1,6 +1,7 @@
 	
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.8.1.min.js"><\/script>')</script>
+	<script src="http://maps.google.com/maps/api/js?sensor=true"></script>
 
         <script src="js/main"></script>
     </body>
