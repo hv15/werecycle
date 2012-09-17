2bcde5fb926165f8e11a64340c5b7782b2263cc3
Andrew Beveridge <andrew.d.beveridge@gmail.com>
2012-09-17 14:42:36 +0100

    implementing sessions 6

--- app/models/map_model.php
+++ app/models/map_model.php
@@ -89,7 +89,7 @@ class map_model extends CI_Model {
 		$row = $query->result_array();
 		$row = $row[0];
 		$html = $row["html_info"];
-		return print_r($row,1);
+		//return print_r($row,1);
 		    
 		    // Output name of outlet
 		    $output .= "<span class='nametitle'>Name</span><br />\n<span class='name'>{$row["outlet_name"]}</span><br /><br />\n";
@@ -131,6 +131,7 @@ class map_model extends CI_Model {
 		    $i=0;
 		    $count=$query->num_rows();
 		    while( $row = $query->result_array() ) {
+			$output = print_r($row,1);
 			$output .= "<span class='recycle_type_{$row['recycle_type']}'>{$row['name']}";
 			$i++; if($i!=$count) {
 				$output .= ", </span>\n";
