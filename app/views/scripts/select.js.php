a3a4f3c9009342e89dd52023928bf460e72dbeab
Andrew Beveridge <andrew.d.beveridge@gmail.com>
2012-09-17 06:37:51 +0100

    gave select change event

--- app/views/scripts/select.js.php
+++ app/views/scripts/select.js.php
@@ -1,4 +1,14 @@
+function recalculateSelected() {
+	$('.category').each(function(index) {
+		$(this).children('.selectedCount').text($(this).next().children(':checked').length);
+	});
+}
+
 $('.category').click(function(index) { 
 	$(this).next().toggle(); 
 	$(this).next().children().toggle();
+});
+
+$('.typeCheckbox').change(function() {
+	recalculateSelected();
 });
