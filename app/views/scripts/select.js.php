e0ecfc9d33319635215aa4fb8d7445165a251a4f
Andrew Beveridge <andrew.d.beveridge@gmail.com>
2012-09-17 06:22:45 +0100

    added script file for select 6

--- app/views/scripts/select.js.php
+++ app/views/scripts/select.js.php
@@ -1,6 +1,7 @@
 $('.category').click(function(index) { 
-	$(this).next().toggle("fast"); 
-	$(this).next().children().each(function(index) {
-	    $(this).toggle("fast");
-	});
+	$(this).next().toggle("fast", function() {
+		$(this).next().children().each(function(index) {
+			$(this).toggle("fast");
+		});
+	}); 
 });
