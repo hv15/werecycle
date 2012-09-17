de63a85c3bb71c8ec56447047306904ab21a40a3
Andrew Beveridge <andrew.d.beveridge@gmail.com>
2012-09-17 07:11:23 +0100

    test map

--- app/controllers/pages.php
+++ app/controllers/pages.php
@@ -29,9 +29,9 @@ class Pages extends CI_Controller {
 		}
 		
 		if($page == 'map') {
-			$segarray = $this->uri->segment_array();
-			unset($segarray[1]);
-			$this->session->set_userdata($segarray);
+			//$segarray = $this->uri->segment_array();
+			//unset($segarray[1]);
+			//$this->session->set_userdata($segarray);
 			$data['categories'] = $this->map_model->get_outlets();
 		}
 		
