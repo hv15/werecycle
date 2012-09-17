15a63bfc602e8c031f33bb8e54f64055b434f468
Andrew Beveridge <andrew.d.beveridge@gmail.com>
2012-09-17 12:04:40 +0100

    implementing sessions 2

--- app/views/scripts/select.js.php
+++ app/views/scripts/select.js.php
@@ -1,5 +1,5 @@
 recalculateSelected();
-$('#debug').text(readCookie('recycle_session'));
+$('#debug').text(readCookie('recyclefinder_session'));
 	
 function recalculateSelected() {
 	$('.category').each(function(index) {
