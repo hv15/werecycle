39da744d6594c12e1a05110816166006389b64d9
Leif Gehrmann <leif.gehrmann@gmail.com>
2012-09-17 21:46:12 +0100

    changed css for info.php

--- app/views/pages/help.php
+++ app/views/pages/help.php
@@ -1,16 +1,22 @@
 	<div class="main-container">
             <div class="main wrapper clearfix">
-            	<img src="/img/logo.png"/>
+            	<img src="/img/logo.png" width="128"/>
 				<p class="title">RecycleFinder</p>
 				<p class="description">An open project</p>
 
-				<p class="contributors">Contributor</p>
+				<p class="contributors">Contributors</p>
 				<ul>
 					<li>Andrew Beveridge</li>
 					<li>Andrew MacKinnon</li>
 					<li>Hans-Nikolai Viessmann</li>
 					<li>Konstantin Devyatov</li>
 					<li>Leif Gehrmann</li>
+				</ul>
+
+				<p class="contributors">We would like to thank...</p>
+				<ul>
+					<li><a href="http://maps.google.com">Google Maps</a></li>
+					<li><a href="http://recycleforscotland.com">RecycleForScotland</a></li>
 				</ul>
             </div> <!-- #main -->
         </div> <!-- #main-container -->
