529c676b07bfd6e69211824d8cbd74806abd1e05
Andrew Beveridge <andrew.d.beveridge@gmail.com>
2012-09-17 03:27:27 +0100

    fixed site url in header footer

--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -2,6 +2,6 @@
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.8.1.min.js"><\/script>')</script>
 
-        <script src="<?php site_url('js/main.js.php'); ?>"></script>
+        <script src="js/main.js.php"></script>
     </body>
 </html>
--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -10,8 +10,8 @@
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width">
 
-        <link rel="stylesheet" href="<?php site_url('css/normalize.min.css'); ?>">
-        <link rel="stylesheet" href="<?php site_url('css/main.css'); ?>">
+        <link rel="stylesheet" href="css/normalize.min.css">
+        <link rel="stylesheet" href="css/main.css">
 
         <script src="<?php site_url('js/vendor/modernizr-2.6.1-respond-1.1.0.min.js'); ?>"></script>
     </head>
