4680d9a212105d8506e9f2c79b3e98997d3770f8
Andrew Beveridge <andrew.d.beveridge@gmail.com>
2012-09-17 12:36:49 +0100

    implementing sessions 3

--- app/views/scripts/select.js.php
+++ app/views/scripts/select.js.php
@@ -1,5 +1,5 @@
 recalculateSelected();
-console.log(readCookie('recyclefinder_session'));
+$('#debug').text($.cookie('recyclefinder_session', { json: true }));
 	
 function recalculateSelected() {
 	$('.category').each(function(index) {
@@ -24,28 +24,3 @@ $('.category').click(function(index) {
 $('.typeCheckbox').change(function() {
 	recalculateSelected();
 });
-
-function createCookie(name,value,days) {
-	if (days) {
-		var date = new Date();
-		date.setTime(date.getTime()+(days*24*60*60*1000));
-		var expires = "; expires="+date.toGMTString();
-	}
-	else var expires = "";
-	document.cookie = name+"="+value+expires+"; path=/";
-}
-
-function readCookie(name) {
-	var nameEQ = name + "=";
-	var ca = document.cookie.split(';');
-	for(var i=0;i < ca.length;i++) {
-		var c = ca[i];
-		while (c.charAt(0)==' ') c = c.substring(1,c.length);
-		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-	}
-	return null;
-}
-
-function eraseCookie(name) {
-	createCookie(name,"",-1);
-}
