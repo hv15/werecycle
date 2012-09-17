3c9e00f721a91fca9f5e1556aca05b552f7963c1
Andrew Beveridge <andrew.d.beveridge@gmail.com>
2012-09-17 03:50:24 +0100

    gave scripts controller a javascript content type

--- app/controllers/scripts.php
+++ app/controllers/scripts.php
@@ -9,7 +9,7 @@ class Scripts extends CI_Controller {
 			show_404();
 		}
 		
-		ob_start(); ob_clean(); header ("Content-type: application/x-javascript");
+		header ("Content-type: application/x-javascript");
 		$this->load->view('scripts/'.$file.'.js.php');
 	}
 	public function vendor($file)
@@ -20,7 +20,7 @@ class Scripts extends CI_Controller {
 			show_404();
 		}
 		
-		ob_start(); ob_clean(); header ("Content-type: application/x-javascript");
+		header ("Content-type: application/x-javascript");
 		$this->load->view('scripts/vendor/'.$file);
 	}
 }
