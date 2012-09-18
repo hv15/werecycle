28cb795d1b5e8e276799972e66868b527666debf
Andrew Beveridge <ab441@hw.ac.uk>
2012-09-18 18:30:55 +0100

    testing new way of handling types in session

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -6,13 +6,12 @@
     <head>
         <title><?=$title?></title>
 		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-        <meta charset="utf-8"/>
-        <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable:no;"/>
+		<meta http-equiv="content-type" content="text/html; charset=UTF-8"/>        
+		<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable:no;"/>
 
         <link rel="stylesheet" href="/css/normalize.min.css"/>
         <link rel="stylesheet" href="/css/main.css"/>
 	
-        <script src="/js/vendor/modernizr-2.6.1-respond-1.1.0.min.js"></script>
         <script src="http://maps.google.com/maps/api/js?sensor=true"></script>
     </head>
     <body>
