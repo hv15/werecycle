22edf16d1ef8ccb0c1ca49da494a0e52d5867f5b
Leif Gehrmann <leif.gehrmann@gmail.com>
2012-09-18 11:46:41 +0100

    modified content in help.php

--- app/views/pages/help.php
+++ app/views/pages/help.php
@@ -1,11 +1,24 @@
 	<div class="main-container">
             <div class="main wrapper clearfix">
 		<a onclick="javascript:history.back()" class="button" id="ButtonBack">&#8617;</a>
-
-            	<img src="/img/logo.png" width="128"/>
+        <img src="/img/logo.png" width="128"/>
 		<p class="title">RecycleFinder</p>
-		<p class="description">An open project</p>
-
+		<p class="description"></p>
+		<p>Help</p>
+		<p>
+			This application is designed to locate recycling outlets
+			in nearby locations. You can select individual items to
+			recycle or recycle in bulk after you specify your location.
+		</p>
+		<p>
+			The map will display all nearby outlets that have facilities
+			to process all types of goods selected. You can find more
+			information about individual outlets by clicking on the markers.
+		</p>
+		<p>To get more information about individual recyclables, tap the 
+			<em>i</em> next to the type. This will provide additional 
+			information, and tips on how to <em>Reduce, Reuse, Recycle</em>.
+		</p>
 		<p class="contributors">Contributors</p>
 		<ul>
 			<li>Beveridge, Andrew</li>
@@ -14,11 +27,8 @@
 			<li>Devyatov, Konstantin</li>
 			<li>Gehrmann, Leif</li>
 		</ul>
-
 		<p><a href="http://goo.gl/etd3F/">Provide Feedback</a></p>
 		<p><a href="https://code.google.com/p/werecycle/">Project Homepage</a></p>
 		<p><a href="https://code.google.com/p/werecycle/wiki/Acknowledgments">Acknowledgements</a></p>
-
-		<p>miaow</p>
             </div> <!-- #main -->
         </div> <!-- #main-container -->
