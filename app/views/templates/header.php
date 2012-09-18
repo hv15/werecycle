6f5ad1a7b141239e5cb9a22ba54ee9bcba5b2581
Andrew Beveridge <ab441@hw.ac.uk>
2012-09-18 18:44:46 +0100

    testing new way of handling types in session

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -1,17 +1,22 @@
 <!DOCTYPE html>
-<html class="page-<?=$page?>">
+<html class="no-js page-<?=$page?>">
     <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <title><?=$title?></title>
-		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-		<meta http-equiv="content-type" content="text/html; charset=UTF-8"/>        
-		<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable:no;"/>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable:no;">
 
-        <link rel="stylesheet" href="/css/normalize.min.css"/>
-        <link rel="stylesheet" href="/css/main.css"/>
+        <link rel="stylesheet" href="/css/normalize.min.css">
+        <link rel="stylesheet" href="/css/main.css">
 	
+        <script src="/js/vendor/modernizr-2.6.1-respond-1.1.0.min.js"></script>
         <script src="http://maps.google.com/maps/api/js?sensor=true"></script>
     </head>
-    <body>
+<!--[if lt IE 7]>      <body class="lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <body class="lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <body class="lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <body> <!--<![endif]-->
         <!--[if lt IE 7]>
             <p class="chromeframe">You are using an outdated browser. <a href="http://browsehappy.com/">Upgrade your browser today</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to better experience this site.</p>
         <![endif]-->
