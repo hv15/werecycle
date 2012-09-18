abd615685f909ba20f41e68395299f46a8648d41
Leif Gehrmann <leif.gehrmann@gmail.com>
2012-09-18 08:25:55 +0100

    changed select.js.php animation

--- app/views/scripts/select.js.php
+++ app/views/scripts/select.js.php
@@ -22,15 +22,22 @@ function createQuery() {
 	});
 }
 
+/*
+	This function used to be a simple toggle button.
+	But now that I added animations, the order in which
+	I toggle matters, so it looks like I have duplicate code.
+*/
 $('.category').click(function(index) { 
-	if(!$(this).hasClass("open"))
+	if(!$(this).hasClass("open")){
 		$(this).addClass("open");
+
 		// opens the categories with animation
 		var types = $(this).next();
 		$(this).next().children().toggle();
 		types.slideToggle('slow');
-	else {
+	} else {
 		$(this).removeClass("open");
+
 		// closes the categories with animation
 		$(this).next().slideToggle('slow');
 		$(this).next().children().toggle();
