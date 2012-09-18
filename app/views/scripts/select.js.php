740ea92fa755426466de745fe8e02d31ac037046
Andrew Beveridge <ab441@hw.ac.uk>
2012-09-18 16:40:19 +0100

    testing new way of handling types in session

--- app/views/scripts/select.js.php
+++ app/views/scripts/select.js.php
@@ -17,7 +17,7 @@ function setTypes() {
 	$.get('/check/'+types, function(data) {
 		if(data>0) {
 			/*alert("There are "+data+" recycle points which fit this selection. Redirecting you to the map...");*/		
-			var newSessionData = encodeURIComponent('{"types_selected":'+typescommas.slice(0, - 1);+'}');
+			var newSessionData = encodeURIComponent('{"types_selected":'+typescommas.slice(0, - 1)+'}');
 			$.get('/setsession/'+newSessionData, function(data){
 				/*window.location.href = '/map';*/
 				alert(data);
