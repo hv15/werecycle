8e581486128f6f7b43627ba55fff135e34ff5e75
Andrew Beveridge <ab441@hw.ac.uk>
2012-09-18 18:45:44 +0100

    testing new way of handling types in session

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html class="no-js page-<?=$page?>">
+<html>
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
@@ -13,10 +13,10 @@
         <script src="/js/vendor/modernizr-2.6.1-respond-1.1.0.min.js"></script>
         <script src="http://maps.google.com/maps/api/js?sensor=true"></script>
     </head>
-<!--[if lt IE 7]>      <body class="lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <body class="lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <body class="lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <body> <!--<![endif]-->
+<!--[if lt IE 7]>      <html class="no-js page-<?=$page?> lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js page-<?=$page?> lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js page-<?=$page?> lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <body class="no-js page-<?=$page?>"> <!--<![endif]-->
         <!--[if lt IE 7]>
             <p class="chromeframe">You are using an outdated browser. <a href="http://browsehappy.com/">Upgrade your browser today</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to better experience this site.</p>
         <![endif]-->
