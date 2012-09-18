feda2c63e8ebb846de097bb57ea317ffaf89b216
Andrew Beveridge <ab441@hw.ac.uk>
2012-09-18 19:24:21 +0100

    testing new way of handling types in session

--- app/views/pages/info.php
+++ app/views/pages/info.php
@@ -1,39 +1,44 @@
 	<div class="main-container">
             <div class="main wrapper clearfix">	
-            	<a href="<?=(isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '/')?>"><div class="button back"><p>&#8617; Return to Map</p></div></a>
-		
-		<span class='nametitle'>Name</span><br />
-		<span class='name'><?=$info['name']?></span><br /><br />
-		<span class='phonetitle'>Phone</span><br />
-		<span class='phone'><?=$info['phone']?></span><br /><br />
-
-		<span class='addresstitle'>Address</span><br />
-		<div class='address'>
-			<a href='http://maps.google.com/maps?q=<?=$info['latitude']?>,<?=$info['longitude']?>' target='_blank'>
-				<?=$info['address']?>
-			</a>
-		</div><br /><br />
-
-		<span class='openhourstitle'>Opening Hours</span><br />
-		<div class='openhours'>
-			<?=$info['openhours']?>
-		</div>
-		
-		<div class="info"><p>Press a category to expand</p></div>
-		<?php foreach($categories as $category_id => $category) { ?>
-			<div class="category" id="category<?=$category_id?>">
-				<div class="categoryToggle"></div>
-				<div class="categoryName"><?=$category['name'];?></div>
-			</div>
-			<div class="types" id="category<?=$category_id?>Types">
-				<?php foreach($category['types'] as $id => $recycle_type) { ?>
-					<div class="type" id="type<?=$id?>">
-						<p class="typeInfoButton">i</p>
-						<p class="typeName"><?=$recycle_type['name']?></p>
-						<div class="typeInfoText"><?=$recycle_type['description']?></div>
+            	<a href="<?=(isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '/')?>">
+					<div class="button back"><p>&#8617; Return to Map</p></div>
+				</a>
+				
+				<div class='nametitle'>Name</div>
+				<div class='name'><?=$info['name']?></div>
+				<br />
+				<div class='phonetitle'>Phone</div>
+				<div class='phone'><?=$info['phone']?></div>
+				<br />
+				<div class='addresstitle'>Address</div>
+				<div class='address'>
+					<a href='http://maps.google.com/maps?q=<?=$info['latitude']?>,<?=$info['longitude']?>' target='_blank'>
+						<?=$info['address']?>
+					</a>
+				</div>
+				<br />
+				<div class='openhourstitle'>Opening Hours</div>
+				<div class='openhours'>
+					<?=$info['openhours']?>
+				</div>
+				<br />
+				<div class="info">
+					<p>Press a category to expand</p>
+				</div>
+				<?php foreach($categories as $category_id => $category) { ?>
+					<div class="category" id="category<?=$category_id?>">
+						<div class="categoryToggle"></div>
+						<div class="categoryName"><?=$category['name'];?></div>
 					</div>
-				<?php } ?>
-			</div>
-		<?php } ?>	
+					<div class="types" id="category<?=$category_id?>Types">
+						<?php foreach($category['types'] as $id => $recycle_type) { ?>
+							<div class="type" id="type<?=$id?>">
+								<p class="typeInfoButton">i</p>
+								<p class="typeName"><?=$recycle_type['name']?></p>
+								<div class="typeInfoText"><?=$recycle_type['description']?></div>
+							</div>
+						<?php } ?>
+					</div>
+				<?php } ?>	
             </div> <!-- #main -->
         </div> <!-- #main-container -->
