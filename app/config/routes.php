6a5049bf13754473ea4c67ec7bf8a41d7208248a
Andrew Beveridge <ab441@hw.ac.uk>
2012-09-18 18:05:08 +0100

    testing new way of handling types in session

--- app/config/routes.php
+++ app/config/routes.php
@@ -40,7 +40,7 @@
 
 $route['js/vendor/(:any)'] = 'scripts/vendor/$1';
 $route['js/(:any)'] = 'scripts/view/$1';
-$route['data'] = 'pages/data';
+$route['data/(:any)'] = 'pages/data';
 $route['check/(:any)'] = 'pages/check/$1';
 $route['getsession'] = 'pages/get_session';
 $route['getsession/(:any)'] = 'pages/get_session';
