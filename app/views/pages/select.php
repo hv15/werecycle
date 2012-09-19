1216c64fed92ce3885af4b198a0b03fe49347922
Leif Gehrmann <leif.gehrmann@gmail.com>
2012-09-19 18:52:50 +0100

    Testing what happens when I add buttons to the types

--- app/views/pages/select.php
+++ app/views/pages/select.php
@@ -25,11 +25,11 @@
 			<div class="types" id="category<?=$category_id?>Types">
 				<?php foreach($category['types'] as $id => $recycle_type) { ?>
 					<div class="type" id="type<?=$id?>">
-						<p class="button roundLeft roundLeft typeCheckboxP">
+						<p class="typeCheckboxP">
 							<input class="typeCheckbox" type="checkbox" <?=(in_array($id,$selectedtypes) ? 'checked="checked"' : '')?> id="checkbox<?=$id?>" name="checkbox<?=$id?>" value="<?=$id?>" />
 						</p>
-						<label for="checkbox<?=$id?>" class="button roundNone typeName"><?=$recycle_type['name']?></label>
-						<p class="button roundRight typeInfoButton">i</p>
+						<label for="checkbox<?=$id?>" class="typeName"><?=$recycle_type['name']?></label>
+						<p class="button typeInfoButton">i</p>
 						<div class="typeInfoText"><?=$recycle_type['description']?></div>
 					</div>
 				<?php } ?>
