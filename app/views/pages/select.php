eef562c01460db78fc0168aea15ba6e712112192
Leif Gehrmann <leif.gehrmann@gmail.com>
2012-09-19 21:33:52 +0100

    Adding alert to the screen

--- app/views/pages/select.php
+++ app/views/pages/select.php
@@ -6,38 +6,42 @@
 	}
 ?>
 	<div class="main-container">
-            <div class="main wrapper clearfix">
-		<div id="info">
-			<p>Select the items you want to recycle</p>
-		</div>
-		
-		<?php 
-		$userdata = $this->session->all_userdata();
-		$selectedtypes  = (isset($userdata['types_selected']) ? explode(',',$userdata['types_selected']) : array(1,7,6,16));
-		foreach($categories as $category_id => $category) { ?>
-			<div class="category" id="category<?=$category_id?>">
-				<div class="categoryToggle"></div>
-				<div class="categoryName"><?=$category['name'];?></div>
-				<div class="categoryCount">
-					<p class="selectedCount">0</p>/<p class="totalCount"><?=count($category['types'])?></p>
-				</div>
+        <div class="main wrapper clearfix">
+			<div id="Info">
+				<p>Press the items you want to recycle</p>
 			</div>
-			<div class="types" id="category<?=$category_id?>Types">
-				<?php foreach($category['types'] as $id => $recycle_type) { ?>
-					<div class="type left" id="type<?=$id?>">
-						<div class="typeCheckboxItem simplebutton roundLeft">
-							<input class="typeCheckbox" type="checkbox" <?=(in_array($id,$selectedtypes) ? 'checked="checked"' : '')?> id="checkbox<?=$id?>" name="checkbox<?=$id?>" value="<?=$id?>" />
-							<label for="checkbox<?=$id?>" class="typeName"><?=$recycle_type['name']?></label>
-						</div>
-						<p class="simplebutton roundRight typeInfoButton">i</p>
-						<div class="typeInfoText"><?=$recycle_type['description']?></div>
+
+			<?php 
+			$userdata = $this->session->all_userdata();
+			$selectedtypes  = (isset($userdata['types_selected']) ? explode(',',$userdata['types_selected']) : array(1,7,6,16));
+			foreach($categories as $category_id => $category) { ?>
+				<div class="category" id="category<?=$category_id?>">
+					<div class="categoryToggle"></div>
+					<div class="categoryName"><?=$category['name'];?></div>
+					<div class="categoryCount">
+						<p class="selectedCount">0</p>/<p class="totalCount"><?=count($category['types'])?></p>
 					</div>
-				<?php } ?>
+				</div>
+				<div class="types" id="category<?=$category_id?>Types">
+					<?php foreach($category['types'] as $id => $recycle_type) { ?>
+						<div class="type left" id="type<?=$id?>">
+							<div class="typeCheckboxItem simplebutton roundLeft">
+								<input class="typeCheckbox" type="checkbox" <?=(in_array($id,$selectedtypes) ? 'checked="checked"' : '')?> id="checkbox<?=$id?>" name="checkbox<?=$id?>" value="<?=$id?>" />
+								<label for="checkbox<?=$id?>" class="typeName"><?=$recycle_type['name']?></label>
+							</div>
+							<p class="simplebutton roundRight typeInfoButton">i</p>
+							<div class="typeInfoText"><?=$recycle_type['description']?></div>
+						</div>
+					<?php } ?>
+				</div>
+			<?php } ?>
+			<div id="SearchButton" class="button" onclick="setTypes();">
+				<p>Search recycling facilities...</p>
 			</div>
-		<?php } ?>
-		
-		<div id="searchButton" class="button" onclick="setTypes();">
-			<p>Search recycling facilities...</p>
-		</div>
-            </div> <!-- #main -->
-        </div> <!-- #main-container -->
+			<div id="Alert">
+				<p class="alertText" id="AlertText"></p>
+				<div id="Cancel" class="button cancel"><p>Cancel</p></div>
+				<div id="Ok" class="button cancel"><p>OK</p></siv>
+			</div>
+        </div> <!-- #main -->
+    </div> <!-- #main-container -->
