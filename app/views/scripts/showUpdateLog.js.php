718de473f2d79b69e3fae7ea2de6459df89f2d0a
Andrew Beveridge <andrew.d.beveridge@gmail.com>
2012-10-21 04:03:50 +0100

    fixed

--- app/views/scripts/showUpdateLog.js.php
+++ app/views/scripts/showUpdateLog.js.php
@@ -2,6 +2,9 @@ $(document).ready(function(){
 	var url = $('#updateLogOutput').html();
 	var refreshId = setInterval(function() {
 		$('#updateLogOutput').load(encodeURI(url));
-		$("#updateLogOutput").attr({ scrollTop: $("#updateLogOutput").attr("scrollHeight") });
+		var psconsole = $('#updateLogOutput');
+		psconsole.scrollTop(
+			psconsole[0].scrollHeight - psconsole.height()
+		);
 	}, 3000);
 });
