1890487f71d594b626ca87d6e21b4ce1bd5c26f5
Andrew Beveridge <andrew.d.beveridge@gmail.com>
2012-10-21 16:03:15 +0100

    started updating get_outlets

--- app/models/map_model.php
+++ app/models/map_model.php
@@ -107,6 +107,8 @@ class map_model extends CI_Model {
 		// Script start time - so we can see how long it takes at various stages
 		$time_start = microtime(true);
 		
+		for($i=0; $i<10; $i++) {
+		
 		// Load ALL OUTLETS and ALL OUTLET RECYCLE TYPES into PHP ARRAYS
 		$sql = "SELECT outlets.outlet_id, outlets.latitude, outlets.longitude FROM outlets";
 		$query = $this->db->query($sql);
@@ -132,7 +134,7 @@ class map_model extends CI_Model {
 			$outlets[$outlets_recycle_types_table_row['outlet_id']]['types'][] = $outlets_recycle_types_table_row['recycle_type'];
 		}
 		
-		
+		}
 		
 		return "<pre>".print_r($outlets,1)."</pre> <br /> Took ". (microtime(true)-$time_start) . " milliseconds, i think";
 		
