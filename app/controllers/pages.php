b4584ed4c33c64a04c8a4233cc375da409e7631c
Andrew Beveridge <andrew.d.beveridge@gmail.com>
2012-10-21 19:23:50 +0100

    started updating get_outlets

--- app/controllers/pages.php
+++ app/controllers/pages.php
@@ -107,7 +107,7 @@ class Pages extends CI_Controller {
 	
 	public function datanew() 
 	{
-		$data['outlets'] = $this->map_model->get_outlets_new("1,2,7,16",55.942,-3.214,12);
+		$data['outlets'] = $this->map_model->get_outlets_new("1,2,7,16",55.942,-3.214,15);
 		$this->load->view('pages/data', $data);
 	}
 	
