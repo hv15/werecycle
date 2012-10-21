6287e27316b56fd90edba1870beb35c133b0dee0
Andrew Beveridge <andrew.d.beveridge@gmail.com>
2012-10-21 03:30:41 +0100

    fixed

--- app/views/pages/update.php
+++ app/views/pages/update.php
@@ -1,2 +1,6 @@
-<a href="/update/startUpdate/recycleForScotland">Start recycleForScotland update</a>
-<a href="/update/showUpdateLog/recycleForScotland">Show recycleForScotland update log</a>
+<br />
+<a href="/update/startUpdate/recycleForScotland">Start recycleForScotland update</a><br />
+<br />
+<a href="/update/showUpdateLog/recycleForScotland">Show recycleForScotland update log</a><br />
+<br />
+<a href="/update/stopUpdate">Stop update</a><br />
